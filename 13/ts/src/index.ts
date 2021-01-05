50d10cd9ac9d671edccca53d13421ef6bd930a23
Marco Grunert <marco+github@grnrt.org>
2021-01-05 16:28:43 +0100

    WIP

--- 13/ts/src/index.ts
+++ 13/ts/src/index.ts
@@ -21,37 +21,58 @@ export function task1(lines:string[], earlDepTime:number) {
 // task 2
 export function task2(linesStr:string[], offset:number) {
 
-    var lines:number[] = []
+    var lines:Map<number, number> = new Map()
     
-    linesStr.map(lineStr => {
-        if (lineStr == "x") {
-            lines.push(0)
-        } else {
-            lines.push(parseInt(lineStr))
+    var maxLine = 0
+    var maxIdx = 0
+
+    var minLine = 0
+    var minIdx = 0
+
+    for (var i = 0; i < linesStr.length; i ++) {
+        if (linesStr[i] != 'x') {
+            var line = parseInt(linesStr[i])
+            if (maxLine < line) {
+                maxLine = line
+                maxIdx = i
+            }
+            if (minLine == 0 || minLine > line) {
+                minLine = line
+                minIdx = i
+            }
+            lines.set(i, line)
         }
-    })
+    }
     
-    var t = offset
-    while(t % lines[0] != 0) {
+    var match = false
+
+    var t = offset 
+
+    while(t % maxLine != 0) {
         t = t + 1
     }
 
-    var match = false
-    while (!match) {
-        match = true
-        for (var i = 0; i < lines.length; i ++) {
-            if (lines[i] > 0) {
-                if (!((t + i) % lines[i] == 0)) {
-                    match = false
-                    break
-                }
-            }
-        }
-        if (match == true) {
-            break
-        }
-        t = t + lines[0]
-    }
-    return t
+    // var match = false
+    // while (!match) {
+    //     itCnt ++
+        
+
+        
+    //     match = true
+    //     for (var i = 0; i < lines.length; i ++) {
+    //         if (lines[i] > 0) {
+    //             if (!((t + i) % lines[i] == 0)) {
+    //                 match = false
+    //                 break
+    //             }
+    //         }
+    //     }
+    //     if (match == true) {
+    //         break
+    //     }
+    //     t = t + maxLine - lines[0]
+    // }
+    // console.log(`${itCnt} iterations`)
+    // return t
 }
 // console.log(`solution (task2) = ${task2("41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,541,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,983,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19".split(","), 100000000000000)}`)
